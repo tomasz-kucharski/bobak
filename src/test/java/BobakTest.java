2bc246836fd60e7ad1e86c3ca7dc459f95d3571f
Tom Kucharski <kucharski.tom@gmail.com>
2010-09-16 18:52:29 +0200

    More Bobak functionality :)

--- src/test/java/BobakTest.java
+++ src/test/java/BobakTest.java
@@ -12,4 +12,19 @@ public class BobakTest {
         String response = new Bobak().sayHello();
         Assert.assertEquals(Bobak.HELLO_BOBAK,response);
     }
+
+    @Test
+    public void shouldSayArghWhenAskedMoreThan3Times() {
+        //given
+        Bobak bobak = new Bobak();
+
+        //when
+        String response;
+        bobak.sayHello();
+        bobak.sayHello();
+        response = bobak.sayHello();
+
+        //then
+        Assert.assertEquals(Bobak.ARGH,response);
+    }
 }
